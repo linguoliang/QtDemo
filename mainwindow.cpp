a08483e6523b2d1a63f5ceb0e85f772a4155861e
linguoliang <linguoliang1313@gmail.com>
2016-01-15 14:09:42 +0800

    lin

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,11 +1,13 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include "QMessageBox"
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    QMessageBox::aboutQt(this);
 }
 
 MainWindow::~MainWindow()
